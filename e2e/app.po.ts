9e50fac19a464f5122cd6f42a3b5183b7bee7aea
Shaul Naim <shaul@Shauls-MacBook-Pro.local>
2017-05-12 10:22:32 +0300

    change  selector name

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class LechaimCliPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('lechaim-app h1')).getText();
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lechaim-app',
   templateUrl: './app.component.html'
 })
 
